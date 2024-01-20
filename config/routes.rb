2d7b4039948960d15af06ee1483447d726247100
portal martin <ptl_martin@yahoo.co.jp>
2024-01-20 22:28:54 +0900

    adding routes: users, bookings and services

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :services do
+    resources :bookings, only: [:new, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :bookings, only: [:index, :show, :destroy] do
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
+
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    resources :services, only: [:index, :show], dependant: :destroy
+  end
 end
