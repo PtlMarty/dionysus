61750122e71c08ee4baf5366ccb622f0eb5f2ea6
portal martin <ptl_martin@yahoo.co.jp>
2024-01-20 17:37:17 +0900

    add validation and association to models

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_hours, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many  :services
+  has_many  :bookings
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
 end
