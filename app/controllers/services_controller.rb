f890ccae3e3116d0c912076c2b456047656cbd1b
Laurice Minamihara <lauricejoie.m@gmail.com>
2024-01-25 21:40:33 +0900

    Connect the new form to appear in index

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,15 +1,12 @@
 class ServicesController < ApplicationController
   def index
     @services = Service.all
+    @service = Service.new
   end
 
   def show
     # GET /services/:id
     @service = Service.find(params[:id])
-  end
-
-  def new
-    @service = Service.new
   end
 
   private
