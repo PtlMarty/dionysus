8078f54b399c0d3b58e73f0b95ec1297f1e58110
MomokoJane Kina <momokokina@gmail.com>
2024-01-25 22:21:05 +0900

    configured who booked me booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = current_user.bookings
+  end
+
+
+  def new
+    @booking = Booking.new
+  end
+
+  # def create
+  #   @booking = Booking.new(booking_params)
+  #   if @booking.save
+  #     flash[:success] = "Booking created"
+  #     redirect_to @booking
+  #   else
+  #     flash.now[:error] = "Booking not created"
+  #     render 'new'
+  #   end
+  # end
+
+  def show
+    @bookings = Booking.find(params[:id])
+  end
+
+
+  private
+
+    def booking_params
+      params.require(:booking).permit(:id)
+    end
+
+
 end
