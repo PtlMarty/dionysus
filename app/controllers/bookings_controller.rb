5138be07a2ebaa6b80cd5a5952db4912ca6017eb
portal martin <ptl_martin@yahoo.co.jp>
2024-01-25 21:08:29 +0900

    Adding accept and decline method to the booking-controller and implement the seed to have some bookings. I correct the error in the booking model validation 'start_date, end_date'

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
 class BookingsController < ApplicationController
+  # as a user i can see all booking request from other users
+  def index
+    @bookings = Booking.all
+    @my_services = Service.where(user: current_user)
+    @my_bookings = Booking.service.where(user: current_user)
+
+
+  end
+
+# accept or reject the booking by clicking on a button
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.status = "accepted"
+    @booking.save
+    redirect_to bookings_path
+  end
+
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.status = "rejected"
+    @booking.save
+    redirect_to bookings_path
+  end
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :service
   belongs_to :user
-  validates :start_time, presence: true
-  validates :end_time, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   validates :comment, presence: true
   validates :status, presence: true
   enum status: { pending: 0, accepted: 1, rejected: 2}
